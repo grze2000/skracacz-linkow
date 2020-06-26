ab07d2a3ffb18eecb3e1c2bef7f41244f8e02e4d
Programista3 <programista333@gmail.com>
2020-06-26 19:48:06 +0200

    Add CORS

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,11 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import Router from './routes/router.ts'
 
 const app = new Application();
 
+app.use(oakCors());
 app.use(Router.routes());
 app.use(Router.allowedMethods());
 
